6f364edbd3179c50be8e26e66afa24e56e1f7e8d
Pavlo Tsyhanok <pavlotsyhanok@gmail.com>
2024-06-28 19:41:08 -0700

    +

--- next.config.js
+++ next.config.js
@@ -25,6 +25,10 @@ const nextConfig = withStoreConfig({
         protocol: "https",
         hostname: "medusa-server-testing.s3.us-east-1.amazonaws.com",
       },
+      {
+        protocol: "https",
+        hostname: "medusa-tutorial-bucket.nyc3.digitaloceanspaces.com",
+      },
     ],
   },
 })
